1c9b05174da1590dcd75e1cfe3139b8e98f3354f
dawei liu <daweilliu@hotmail.com>
2017-12-24 13:49:25 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var app = express();
 app.use(bodyParser.json()); // express use middle ware
 
 app.post('/todos', (req, res) => {
+  
     var todo = new Todo({
         text: req.body.text
     });
